2d54d837a8eb39cc0b12814c02bf738d2a63a79d
Nico Wernli <nico.wernli@gmail.com>
2018-05-23 11:34:38 +0100

    feature: create a fake database connection middleware

---
+++ src/core/database/index.js
@@ -0,0 +1,23 @@
+const fakeConnection = () => {
+  return Promise.resolve({
+    close() {
+      return null;
+    }
+  });
+};
+
+/*
+ * Here we should add the connection to your database.
+ * Once you have the connection add that connection to
+ * the request and remember to close it when the response
+ * if finished.
+ */
+export default () => (req, res, next) => {
+  fakeConnection().
+    then((connection) => {
+      res.on('finish', () => connection.close());
+      req.locals.db = connection;
+      next();
+    }).
+    catch((err) => next(err));
+};
