34de2efc8fed3c99542017805d85f5f0a8369d16
Nico Wernli <nico.wernli@gmail.com>
2018-05-23 11:34:56 +0100

    refactor: remove cookieparser and add the database middleware

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ import morgan from 'morgan';
 import timeout from 'connect-timeout';
 import responseTime from 'response-time';
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
 import log from '@core/logger';
+import database from '@core/database';
 import routes from './routes';
 
 const PORT = config.get('server.port');
@@ -18,13 +18,30 @@ const app = express();
 app.use(helmet());
 app.use(compression());
 app.use(morgan('dev'));
-app.use(cookieParser());
 app.use(bodyParser.json({ limit: config.get('server.bodyParserLimit') }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(timeout(config.get('server.timeout')));
 app.use(responseTime());
+app.use(database());
 
 app.use('/', routes);
+
+// 404 hanlder
+app.use((req, res, next) => {
+  const err = new Error('404 Not Found');
+  err.code = 404;
+  next(err);
+});
+
+// Error hanlder
+app.use((err, req, res) => {
+  err.code = err.status || err.code || 500;
+  res.status(err.code).json({
+    code: err.code,
+    msg: err.message
+  });
+});
+
 app.listen(PORT, () => log.info(`${CLIENT.name} running on port ${PORT}`));
 
 export default app;
