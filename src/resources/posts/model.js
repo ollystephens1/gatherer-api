f79cf33b7e2da35fc1d00468bec8d28dd2805dba
Nico Wernli <nico.wernli@gmail.com>
2018-05-29 10:30:19 +0100

    feat: add mongo resource model

--- src/resources/posts/model.js
+++ src/resources/posts/model.js
@@ -6,8 +6,13 @@ export default {
   count() {
     return Promise.resolve(100);
   },
-  find() {
-    return fetch(baseUrl).then(res => res.json());
+  find(filters, limit, page = 1) {
+    return fetch(baseUrl)
+      .then(res => res.json())
+      .then((posts) => {
+        const skip = (page * limit) - limit;
+        return posts.slice(skip, skip + limit);
+      });
   },
   findOne(id) {
     return fetch(`${baseUrl}/${id}`)
