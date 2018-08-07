0ffc8842f01cdc02ae3c9ec51dc0f37cab064f94
ollystephens1 <ostephens88@hotmail.co.uk>
2018-08-07 16:04:35 +0200

    feat: user model updated

--- src/modules/users/models/user.model.js
+++ src/modules/users/models/user.model.js
@@ -2,8 +2,12 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
 	{
-		name: { type: String, required: true },
-		description: { type: String }
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String },
+    description: { type: String },
+    password: { type: String, required: true },
+    avatar: { type: String }
 	},
 	{ timestamps: true, toJSON: { virtuals: true } }
 );
